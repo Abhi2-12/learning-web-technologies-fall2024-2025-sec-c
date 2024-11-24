e9be4eef38147497c0b0e1a3e52f2c4f30005b7d
Abhi2-12 <167578103+Abhi2-12@users.noreply.github.com>
2024-11-24 17:05:18 +0600

    home.php

--- lab_exam/home.php
+++ lab_exam/home.php
@@ -9,7 +9,12 @@
     <title>Home</title>
 </head>
 <body>
-        <h1>Welcome Home!</h1>
-        <a href='logout.php'>logout </a>
+    <h1>Welcome, <?= htmlspecialchars($user['name']); ?>!</h1>
+    <p><strong>Email:</strong> <?= htmlspecialchars($user['email']); ?></p>
+    <p><strong>Gender:</strong> <?= htmlspecialchars($user['gender']); ?></p>
+    <p><strong>Date of Birth:</strong> <?= htmlspecialchars($user['dob']); ?></p>
+    <p><strong>Blood Group:</strong> <?= htmlspecialchars($user['blood_group']); ?></p>
+    <p><strong>Degrees:</strong> <?= htmlspecialchars($user['degrees']); ?></p>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
