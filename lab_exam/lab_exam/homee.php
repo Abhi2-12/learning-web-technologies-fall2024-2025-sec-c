eca3904c96b8fc9a4b7b76dad28210312b793b23
Abhi2-12 <167578103+Abhi2-12@users.noreply.github.com>
2024-11-24 17:06:10 +0600

    homee.php

--- lab_exam/lab_exam/homee.php
+++ lab_exam/lab_exam/homee.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
 
-// Check if the user is logged in
 if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     header("Location: login.html");
     exit();
 }
 
-// Retrieve the user data from the session
 $user = $_SESSION['user'];
 ?>
 
